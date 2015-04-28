4e2b37198accd99b356fbec3dead1f35befdd086
Kosta Rashev <kosta.rashev@gmail.com>
2015-04-27 21:15:11 -0700

    Add current year to footer copyright

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -2,7 +2,17 @@
     <div class="container">
         <div class="row">
             <ul class="footer-text">
-                <li class="footer-copyright"><strong>&copy; Kosta Rashev</strong></li>
+                <li class="footer-copyright">
+                    <strong>
+                        &copy;
+                        <?php
+                            $copyYear = 2015;
+                            $currentYear = date('Y');
+                            echo $copyYear . (($copyYear != $currentYear) ? ' - ' . $currentYear : '');
+                        ?>
+                        Kosta Rashev
+                    </strong>
+                </li>
                 <li class="footer-github"><a href="https://github.com/kr85" class="pull-right"><i class="fa fa-github fa-2x"></i></a></li>
                 <li class="footer-linkin"><a href="https://www.linkedin.com/in/kostarashev" class="pull-right"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
                 <li class="footer-google-plus"><a href="https://plus.google.com/116911418217949761063/posts" class="pull-right"><i class="fa fa-google-plus-square fa-2x"></i></a></li>
