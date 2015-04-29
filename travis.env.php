d725e09c777d69b1026ba10567aacfe0d42d1c5a
Kosta Rashev <kosta.rashev@gmail.com>
2015-04-28 17:51:29 -0700

    Modify travis ci config

--- travis.env.php
+++ travis.env.php
@@ -1,7 +1,7 @@
 <?php
     return [
         'DB_HOST'         => '127.0.0.1',
-        'DB_NAME'         => 'nook',
+        'DB_NAME'         => 'nook_testing',
         'DB_USERNAME'     => 'root',
         'DB_PASSWORD'     => ''
     ];
